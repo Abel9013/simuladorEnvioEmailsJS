fde99df7feb154e5a2b4e91a91c6ded5e9bb1334
Abel <90abel.avr@gmail.com>
2023-04-17 11:10:38 -0300

    add select dom and events

---
+++ js/app.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+  // Seleccionar elementos de la interfaz
+  const inputEmail = document.querySelector('#email')
+  const inputAsunto = document.querySelector('#asunto')
+  const inputMensaje = document.querySelector('#mensaje')
+  
+  // Asignar eventos
+  inputEmail.addEventListener('blur',validar)
+  inputAsunto.addEventListener('blur',validar)
+  inputMensaje.addEventListener('blur',validar)
+
+  function validar(e){
+    console.log(e.target.value);
+  }
+
+} )
